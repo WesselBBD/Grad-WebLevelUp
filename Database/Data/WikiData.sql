21ab1c0abb28d112b67eb44568069c3695666336
Paulo NGOVE <paulon@bbd.co.za>
2022-05-04 15:08:04 +0200

    wiki dummy data

---
+++ Database/Data/WikiData.sql
@@ -0,0 +1,18 @@
+USE OnlyFansDB
+GO
+
+INSERT INTO [dbo].[Wiki] (WikiId,AssoiciatedFan,WritenByUser,URi,Title,Content)
+VALUES
+  ('C576152D-879B-BF4E-6874-2F9BB38E25DF','88BFB08D-18E9-410C-96DE-020070CFD08A','006EBF67-9611-4829-BB60-003E00CB01C4','https://zoom.us/sub/cars?k=0','nec, eleifend','ornare lectus justo eu arcu. Morbi sit amet massa. Quisque porttitor eros nec tellus. Nunc lectus pede, ultrices a, auctor'),
+  ('61ED1573-7A35-72B6-485E-7849925D0B58','B7D585A0-1FA4-427E-AC43-031DF5D81560','894D3AE5-047C-4841-B6BF-00DD5C5D1748','https://naver.com/settings?q=4','diam at','Vivamus sit amet risus. Donec egestas. Aliquam nec enim. Nunc ut erat. Sed nunc est, mollis non, cursus non, egestas'),
+  ('E7E48B8E-85B8-EBD7-3873-5A9F82EE696F','06ED1106-A1E9-47AB-830A-03DAF04FE160','8B38B495-31AF-42FF-B39C-046C7A618AA6','http://twitter.com/en-us?q=0','sagittis placerat.','mauris, rhoncus id, mollis nec, cursus a, enim. Suspendisse aliquet, sem ut cursus luctus, ipsum leo elementum sem, vitae aliquam'),
+  ('56F13A4A-C93D-BC87-3D8D-5CC98890F78E','A155ADE0-2842-4969-B95D-0682FA5275E2','B3CD9222-928E-4104-836C-05DD3853D724','http://yahoo.com/sub?q=11','congue. In','Nullam ut nisi a odio semper cursus. Integer mollis. Integer tincidunt aliquam arcu. Aliquam ultrices iaculis odio. Nam interdum enim'),
+  ('8F648AC6-C3FF-C304-A5B9-7E3EE46914D7','E46E0F86-9849-4489-B5B2-075813F3B55C','6F07A5A2-BFEF-4EF1-9998-06198C177269','http://walmart.com/sub?g=1','lorem ac','accumsan laoreet ipsum. Curabitur consequat, lectus sit amet luctus vulputate, nisi sem semper erat, in consectetuer ipsum nunc id enim.'),
+  ('9FAC593B-E70A-15B6-B61B-442D07C856D3','03FC1B3E-7616-442C-96C7-0889DD705450','5958EDA6-D272-42FE-8539-0CA3790F4B20','https://walmart.com/group/9?page=1&offset=1','nunc sed','non, bibendum sed, est. Nunc laoreet lectus quis massa. Mauris vestibulum, neque sed dictum eleifend, nunc risus varius orci, in'),
+  ('60561B82-4619-279C-67B6-4317846D1314','9A094659-C12C-4D5D-8043-098EEA374408','478713F5-D61D-4438-AC46-0D805E8E91A0','https://youtube.com/en-us?search=1','eu neque','Curabitur massa. Vestibulum accumsan neque et nunc. Quisque ornare tortor at risus. Nunc ac sem ut dolor dapibus gravida. Aliquam'),
+ ('F7720634-389A-5163-EA1A-6DC2216E97B4','E8B7AEF7-30AB-4AED-89D1-170F80453110','9DE5C0C1-7D91-4F19-8FC3-0D9F0C32327B','http://reddit.com/fr?g=1','sapien. Cras','placerat eget, venenatis a, magna. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam laoreet, libero et tristique pellentesque, tellus'),
+  ('0AF2585B-963D-3B80-79E9-4E0BE5BE155F','BE601B9C-2026-490E-B9B1-185A94207A8D','17240074-4574-4F3E-B3F8-0F28E44E65DC','https://baidu.com/one?search=1&q=de','sit amet','Donec est. Nunc ullamcorper, velit in aliquet lobortis, nisi nibh lacinia orci, consectetuer euismod est arcu ac orci. Ut semper'),
+  ('2CEB3BE1-0851-6826-B1BC-C137FA89432D','6F3888FE-4714-4698-B9B3-1BB30A2CFA42','490E006F-598A-4F93-A434-15D3707AE03C','https://pinterest.com/site?q=0','ligula eu','consectetuer euismod est arcu ac orci. Ut semper pretium neque. Morbi quis urna. Nunc quis arcu vel quam dignissim pharetra.');
+
+
+GO
