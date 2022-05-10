64f91560d40f8c9502e6bededd84f2ee64398734
RushilDaya-BBD <rushild@bbd.co.za>
2022-05-10 14:19:22 +0200

    Update BrandData.sql

--- Database/Data/BrandData.sql
+++ Database/Data/BrandData.sql
@@ -2,7 +2,7 @@ USE OnlyFansDB;
 GO
 
 INSERT INTO [dbo].[Brand] ([BrandId], [BrandName])
-VALUES 	(1, 'Smasung'),
+VALUES 	(1, 'Samsung'),
         (2, 'Panasonic'),
         (3, 'LG'),
         (4, 'Corsair'),
