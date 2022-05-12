9ca7dea8ead34a0276419b40155d94fd0a1d6164
Wessel Scholtz <wessels@bbd.co.za>
2022-05-12 17:16:34 +0200

    Footer Added

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,10 +1,13 @@
 import { createStars } from "./components/stars/stars.js";
 import { createCard } from "./components/card/card.js";
-import { createHeader } from "./components/header/Header.js";
+import { createHeader } from "./components/header/header.js";
+import { createFooter } from "./components/footer/footer.js";
 
 const [header] = document.getElementsByTagName("header");
-
 header.append(... createHeader().children);
+
+const [footer] = document.getElementsByTagName("footer");
+footer.append(... createFooter().children);
 
 /*
 const button = document.getElementById("add-star");
@@ -17,6 +20,8 @@ button.addEventListener("click", function () {
 });
 */
 
+const [main] = document.getElementsByTagName("main");
+
 // creates two cards - Featured and Fan of the Week
 window.addEventListener("load", function() {
   const card = createCard("ceiling.jpg", "You know what they say about a", "small package");
@@ -27,7 +32,6 @@ window.addEventListener("load", function() {
   const stars = createStars(Math.round(Math.random() * 5)); // test to add star component to card
   card.appendChild(stars);
   
-  const [body] = document.getElementsByTagName("body");
-  body.appendChild(card);
-  body.appendChild(card1);
+  main.appendChild(card);
+  main.appendChild(card1);
 });
