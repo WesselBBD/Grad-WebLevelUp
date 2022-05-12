2231cf0d3ca6dcf5919b472d0c9211581c36845c
Fabio Sousa Vieira <fabio.sousavieira@vcontractor.co.za>
2022-05-12 07:23:30 +0200

    Initial setup of Slider component

---
+++ Frontend/components/slider/slider.js
@@ -0,0 +1,40 @@
+import { setAttributes } from "../../utils/helpers.js";
+
+const createSlider = (
+  title,
+  { initialValue = 0, min = 0, max = 100, step = 10, onInput = () => {} }
+) => {
+  const sliderBox = document.createElement("section");
+  sliderBox.classList.add("slider-box");
+
+  const label = document.createElement("label");
+  const mark = document.createElement("mark");
+
+  mark.innerText = initialValue;
+  label.innerText = `${title}: `;
+  label.appendChild(mark);
+
+  const input = document.createElement("input");
+
+  setAttributes(input, {
+    type: "range",
+    id: title,
+    name: title,
+    min,
+    max,
+    step,
+    value: initialValue,
+  });
+
+  input.oninput = (event) => {
+    mark.innerText = event.target.value;
+    onInput(event.target.value);
+  };
+
+  sliderBox.appendChild(label);
+  sliderBox.appendChild(input);
+
+  return sliderBox;
+};
+
+export { createSlider };
