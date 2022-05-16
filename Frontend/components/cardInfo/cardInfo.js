ef7e3c725ba89bc02991218f9af4d9ad9891ee77
RushilDaya-BBD <rushild@bbd.co.za>
2022-05-16 17:12:26 +0200

    Proposed query process

--- Frontend/components/cardInfo/cardInfo.js
+++ Frontend/components/cardInfo/cardInfo.js
@@ -3,6 +3,13 @@ const createCardInfo = (card, fanID) => {
     const cardInfo = document.createElement("section");
     cardInfo.classList.add("cardInfo")
     // Query database, use FanID and get required info
+    // Query-fanID
+    // Get the Model and MAxRPM from Fan table
+    // WikiContent from Wiki table
+
+    // NB: Can create view for these 3 columns
+
+
     let info = "FAN INFO" ;
 
     let infoText = document.createTextNode(info);
