36a137eeda4127f290238816eb5bbac26a0a7faf
Wessel Scholtz <wessels@bbd.co.za>
2022-05-17 09:26:15 +0200

    New db who dis?

---
+++ Database/Data/MaterialData.sql
@@ -0,0 +1,10 @@
+USE `OnlyFansDB`;
+
+INSERT INTO `Material` (`MaterialId`, `MaterialName`, `MaterialBubbleHex`)
+VALUES 	(1, 'Cream White', x'fdfbec'),
+	(2, 'Gold', x'e8be4c'),
+	(3, 'Cedar Wood', x'96735a'),
+	(4, 'Anodised Aluminum', x'8c8583'),
+	(5, 'Retro Cyan', x'578e92'),
+	(6, 'Plastic Black', x'332f2e'),
+	(7, '70s Orange', x'e56e0f');
