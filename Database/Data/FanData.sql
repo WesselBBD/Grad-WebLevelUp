c3ce51f0bef7f322f733017b082f1886ed576460
Wessel Scholtz <wessels@bbd.co.za>
2022-05-17 16:42:33 +0200

    Added Desk Fans

--- Database/Data/FanData.sql
+++ Database/Data/FanData.sql
@@ -5,4 +5,10 @@ VALUES	('Helix Whirler', 1, 160, 5234, 1450, 'https://commons.wikimedia.org/wiki
 	('Bento Fanimation', 1, 157, 4774, 1321, 'https://commons.wikimedia.org/wiki/File:Test.png', 3, 4),
 	('Sunbeam', 1, 200, 4978, 1610, 'https://commons.wikimedia.org/wiki/File:Test.png', 5, 5),
 	('M.BLACK/457', 1, 457, 8972, 1780, 'https://commons.wikimedia.org/wiki/File:Test.png', 6, 6),
-	('Vindair', 1, 190, 4566, 1700, 'https://commons.wikimedia.org/wiki/File:Test.png', 7, 7);
+	('Vindair', 1, 190, 4566, 1700, 'https://commons.wikimedia.org/wiki/File:Test.png', 7, 7),
+	('Metl Wind', 2, 530, 2300, 235, 'https://commons.wikimedia.org/wiki/File:Test.png', 8, 8),
+	('WhtTsh 400', 2, 250, 1606, 195, 'https://commons.wikimedia.org/wiki/File:Test.png', 9, 9),
+	('Tubelet V2', 2, 432, 2452, 231, 'https://commons.wikimedia.org/wiki/File:Test.png', 10, 8),
+	('Old Timer A19', 2, 800, 2999, 169, 'https://commons.wikimedia.org/wiki/File:Test.png', 12, 11),
+	('"Little One" Desk Fan', 2, 230, 1843, 124, 'https://commons.wikimedia.org/wiki/File:Test.png', 13, 14),
+	('WhtTsh 350', 2, 250, 1530, 196, 'https://commons.wikimedia.org/wiki/File:Test.png', 9, 9);
--- Database/Data/MaterialData.sql
+++ Database/Data/MaterialData.sql
@@ -7,4 +7,11 @@ VALUES 	(1, 'Cream White', x'fdfbec'),
 	(4, 'Anodised Aluminum', x'8c8583'),
 	(5, 'Retro Cyan', x'578e92'),
 	(6, 'Plastic Black', x'332f2e'),
-	(7, '70s Orange', x'e56e0f');
+	(7, '70s Orange', x'e56e0f'),
+	(8, 'Stainless Steel', x'bdbcba'),
+	(9, 'White Plastic', x'eaeaea'),
+	(10, 'Cyan Stainless Steel', x'19494e'),
+	(11, 'Peach Leather', x'fdb39b'),
+	(12, '60s Amber', x'f8d9a8'),
+	(13, 'Pinkstained Steel', x'a57e88'),
+	(14, 'Yellow Leather', x'f5aa7b');
