a939ddbdf8882c4d29db50198e85be0cad536436
RushilDaya-BBD <rushild@bbd.co.za>
2022-05-18 17:03:26 +0200

    added 404 page

--- Frontend/pages/404/404Page.js
+++ Frontend/pages/404/404Page.js
@@ -1,4 +1,4 @@
-import { createHeader } from "/components/header/header.js";
+import { createHeader } from "../../components/header/header.js";
 import { createFooter } from "../../components/footer/footer.js";
 
 
@@ -13,6 +13,27 @@ const [main] = document.getElementsByTagName("main");
 window.addEventListener("load", function() {
 
     const rotateFanSection = this.document.createElement("section");
+    rotateFanSection.setAttribute("id", "fan");
+
+    const error = document.createElement("p");
+    error.setAttribute("id","errorText");
+    let textEr = this.document.createTextNode("ERROR 4");
+    error.appendChild(textEr);
+
+    const fanImg= this.document.createElement("img");
+    fanImg.setAttribute("src","/assets/imgs/icon-ani.svg");
+    fanImg.setAttribute("id","fanImg");
+    error.appendChild(fanImg);
+
+    const f=this.document.createTextNode("4");
+    error.appendChild(f);
+
+ 
+
+
+    rotateFanSection.appendChild(error);
+  
+
 
     
 
