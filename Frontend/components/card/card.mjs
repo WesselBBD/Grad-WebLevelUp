4dd5ae6f39d2a2796ef9f32ffc7be273e1765677
TiaanTerblanche <tiaant@bbd.co.za>
2022-05-19 15:01:43 +0200

    added class names for components

--- Frontend/components/card/card.mjs
+++ Frontend/components/card/card.mjs
@@ -4,12 +4,15 @@ const createCard = (cardImage, cardTitle , cardHeading = '', cardSubheading = ''
     card.style.backgroundImage = "url('assets/imgs/fan-categories/" + cardImage + "')";
     
     const title = document.createElement("h1");
+    title.classList.add("title")
     let titleText = document.createTextNode(cardTitle);
     title.appendChild(titleText);
+    
     card.appendChild(title);
 
     if (cardHeading != '') {
         const heading = document.createElement("h2");
+        heading.classList.add("heading")
         let headingText = document.createTextNode(cardHeading);
         heading.appendChild(headingText);
         card.appendChild(heading);
@@ -17,6 +20,7 @@ const createCard = (cardImage, cardTitle , cardHeading = '', cardSubheading = ''
 
     if (cardSubheading != '') {
         const subheading = document.createElement("h3");
+        subheading.classList.add("subheading")
         let subheadingText = document.createTextNode(cardSubheading);
         subheading.appendChild(subheadingText);
         card.appendChild(subheading);
