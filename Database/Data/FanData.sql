7c1e1925e374739caa190b6957efc1bf8bc195ba
Wessel Scholtz <wessels@bbd.co.za>
2022-05-19 11:01:08 +0200

    Fan data uris updated

--- Database/Data/FanData.sql
+++ Database/Data/FanData.sql
@@ -1,14 +1,14 @@
 USE `OnlyFansDB`;
 
 INSERT INTO `Fan` (`FanName`, `CategoryId`, `FanRPM`,  `FanCFM`, `FanSpanMM`, `FanImageURL`, `FanBaseColour`, `FanAccentColour`)
-VALUES	('Helix Whirler', 2, 160, 5234, 1450, 'https://commons.wikimedia.org/wiki/File:Test.png', 1, 2),
-	('Bento Fanimation', 2, 157, 4774, 1321, 'https://commons.wikimedia.org/wiki/File:Test.png', 3, 4),
-	('Sunbeam', 2, 200, 4978, 1610, 'https://commons.wikimedia.org/wiki/File:Test.png', 5, 5),
-	('M.BLACK/457', 2, 457, 8972, 1780, 'https://commons.wikimedia.org/wiki/File:Test.png', 6, 6),
-	('Vindair', 2, 190, 4566, 1700, 'https://commons.wikimedia.org/wiki/File:Test.png', 7, 7),
-	('Metl Wind', 3, 530, 2300, 235, 'https://commons.wikimedia.org/wiki/File:Test.png', 8, 8),
-	('WhtTsh 400', 3, 250, 1606, 195, 'https://commons.wikimedia.org/wiki/File:Test.png', 9, 9),
-	('Tubelet V2', 3, 432, 2452, 231, 'https://commons.wikimedia.org/wiki/File:Test.png', 10, 8),
-	('Old Timer A19', 3, 800, 2999, 169, 'https://commons.wikimedia.org/wiki/File:Test.png', 12, 11),
-	('"Little One" Desk Fan', 3, 230, 1843, 124, 'https://commons.wikimedia.org/wiki/File:Test.png', 13, 14),
-	('WhtTsh 350', 3, 250, 1530, 196, 'https://commons.wikimedia.org/wiki/File:Test.png', 9, 9);
+VALUES	('Helix Whirler', 2, 160, 5234, 1450, '/assets/fans/Ceiling Fans/CeilingFan1.png', 1, 2),
+	('Bento Fanimation', 2, 157, 4774, 1321, '/assets/fans/Ceiling Fans/CeilingFan2.png', 3, 4),
+	('Sunbeam', 2, 200, 4978, 1610, '/assets/fans/Ceiling Fans/CeilingFan3.png', 5, 5),
+	('M.BLACK/457', 2, 457, 8972, 1780, '/assets/fans/Ceiling Fans/CeilingFan4.png', 6, 6),
+	('Vindair', 2, 190, 4566, 1700, '/assets/fans/Ceiling Fans/CeilingFan5.png', 7, 7),
+	('Metl Wind', 3, 530, 2300, 235, '/assets/fans/Desk Fans/DeskFan1.png', 8, 8),
+	('WhtTsh 400', 3, 250, 1606, 195, '/assets/fans/Desk Fans/DeskFan2.png', 9, 9),
+	('Tubelet V2', 3, 432, 2452, 231, '/assets/fans/Desk Fans/DeskFan3.png', 10, 8),
+	('Old Timer A19', 3, 800, 2999, 169, '/assets/fans/Desk Fans/DeskFan4.png', 12, 11),
+	('"Little One" Desk Fan', 3, 230, 1843, 124, '/assets/fans/Desk Fans/DeskFan5.png', 13, 14),
+	('WhtTsh 350', 3, 250, 1530, 196, '/assets/fans/Desk Fans/DeskFan6.png', 9, 9);
