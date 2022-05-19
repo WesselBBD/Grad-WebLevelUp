4b0e0bad5121aa8b064167cc377fee3edd508283
Fabio Sousa Vieira <fabio.sousavieira@vcontractor.co.za>
2022-05-19 13:00:29 +0200

    test

--- Frontend/components/logo/logo.mjs
+++ Frontend/components/logo/logo.mjs
@@ -10,10 +10,6 @@ const createLogoBox = (id) => {
   logoBox.appendChild(logo);
 
   return logoBox;
-<<<<<<< Updated upstream:Frontend/components/logo/logo.mjs
-}
-=======
 };
->>>>>>> Stashed changes:Frontend/components/logo/logo.js
 
 export { createLogoBox };
