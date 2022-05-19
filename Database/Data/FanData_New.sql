65adaa452aabfbebf257e50681348578e2c62355
Paulo NGOVE <paulon@bbd.co.za>
2022-05-19 10:57:25 +0200

    fan dummy data

---
+++ Database/Data/FanData_New.sql
@@ -0,0 +1,28 @@
+
+INSERT INTO OnlyFansDB.Fan (FanName ,CategoryId, FanRPM, FanCFM, FanSpanMM, FanImageURL, FanBaseColour, FanAccentColour)
+VALUES
+  ('Mini Venit', 3, 522, 3650, 1255, 'https://commons.wikimedia.org/wiki/File:Test.png', 2, 1),
+   ('Micro Cooler', 3, 250, 2280, 2021, 'https://commons.wikimedia.org/wiki/File:Test.png', 10, 5),
+   ('Pircrote', 3, 750, 1122, 2255, 'https://commons.wikimedia.org/wiki/File:Test.png', 8, 13),
+   ('DP-200 Joint', 3, 600, 6056, 1255, 'https://commons.wikimedia.org/wiki/File:Test.png', 12, 5),
+   ('Numlock', 3, 320, 1788, 1521, 'https://commons.wikimedia.org/wiki/File:Test.png', 1, 14),
+    ('Meta CN', 4, 455, 3650, 5512, 'https://commons.wikimedia.org/wiki/File:Test.png', 4, 9),
+   ('Bridge Nate', 2, 101, 1998, 3255, 'https://commons.wikimedia.org/wiki/File:Test.png', 2, 6),
+   ('VB-800', 1, 522, 4700, 1988, 'https://commons.wikimedia.org/wiki/File:Test.png', 6, 3),
+   ('Power Cooler', 4, 801, 2011, 5521, 'https://commons.wikimedia.org/wiki/File:Test.png', 7, 1),
+   ('18 Block B', 6, 780, 3255, 5822, 'https://commons.wikimedia.org/wiki/File:Test.png', 11, 7),
+    ('Vintage TY', 6, 960, 2369, 9800, 'https://commons.wikimedia.org/wiki/File:Test.png', 12, 13),
+   ('PC-RT FG-50', 5, 560, 7414, 8700, 'https://commons.wikimedia.org/wiki/File:Test.png', 4, 8),
+   ('TechNine', 4, 499, 4511, 8000, 'https://commons.wikimedia.org/wiki/File:Test.png', 9, 3),
+   ('Dent GT', 5, 879, 3256, 3690, 'https://commons.wikimedia.org/wiki/File:Test.png', 3, 7),
+   ('Zero Power', 5, 231, 1569, 2500, 'https://commons.wikimedia.org/wiki/File:Test.png', 10, 11),
+    ('Tech VP-900', 4, 123, 3200, 1477, 'https://commons.wikimedia.org/wiki/File:Test.png', 11, 5),
+   ('Silver Inn', 6, 328, 4789, 8947, 'https://commons.wikimedia.org/wiki/File:Test.png', 3, 8),
+   ('PK-400', 6, 569, 1253, 2588, 'https://commons.wikimedia.org/wiki/File:Test.png', 6, 1),
+   ('Malrovine', 6, 500, 3655, 4500, 'https://commons.wikimedia.org/wiki/File:Test.png', 9, 10),
+   ('Vingate Standby', 5, 701, 2001, 5666, 'https://commons.wikimedia.org/wiki/File:Test.png', 2, 3),
+    ('Snaps Cooler', 5, 698, 9861, 1021, 'https://commons.wikimedia.org/wiki/File:Test.png', 2, 18),
+   ('GT Obs', 4, 252, 1011, 3100, 'https://commons.wikimedia.org/wiki/File:Test.png', 5, 8),
+   ('Interware DS', 5, 255, 3135, 2551, 'https://commons.wikimedia.org/wiki/File:Test.png', 5, 6),
+   ('DP-250 Joint', 4, 150, 4000, 1512, 'https://commons.wikimedia.org/wiki/File:Test.png', 4, 1),
+   ('Mini Venit', 4, 155, 6500, 7890, 'https://commons.wikimedia.org/wiki/File:Test.png', 2, 11);
