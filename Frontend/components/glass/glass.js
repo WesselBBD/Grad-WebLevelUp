f5bcbcc462c680dfbbef52dd458178cadce10904
TiaanTerblanche <tiaant@bbd.co.za>
2022-05-19 09:43:48 +0200

    Update glass.js

--- Frontend/components/glass/glass.js
+++ Frontend/components/glass/glass.js
@@ -2,7 +2,6 @@ import { createStars } from "../stars/stars.js";
 
 const createGlass = (card) => {
     const glass = document.createElement("article");
-    //const card = document.getElementById(cardID);
     card.prepend(glass)
     glass.classList.add("glass");
     glass.style.backgroundImage = card.style.backgroundImage
@@ -12,46 +11,22 @@ const createGlass = (card) => {
     glass.querySelector('.title').innerHTML = card.getElementsByClassName("title")[1].innerHTML
     card.getElementsByClassName('title')[1].style.display = "none";  
     if (card.getElementsByClassName('heading')[0] != null) {
-      //  const heading = card.querySelector('.heading');
-      //const heading = document.createElement("h2");
-      //heading.classList.add("heading");
-     // glass.appendChild(heading);
-     // glass.querySelector('.heading').innerHTML = card.querySelector('.heading').innerHTML
-     // glass.appendChild(card.querySelector('.heading').innerHTML);
+
         card.getElementsByClassName('heading')[0].style.display = "none";  
+
     }
 
     if (card.getElementsByClassName('subheading')[0] != null) {
-     // const subheading = document.createElement("h3");
-     // subheading.classList.add("subheading");
-     // if (qfunc == 0) {
-     // glass.appendChild(subheading);
-     // glass.querySelector('.subheading').innerHTML = card.querySelector('.subheading').innerHTML;
-     // } 
-     // else {
-     //   let textdata = document.createTextNode(qfunc);
-     //   subheading.appendChild(textdata);
-     //   glass.appendChild(subheading);
+
      card.getElementsByClassName('subheading')[0].style.display = "none";  
       }
 
       if (card.getElementsByClassName('button')[0] != null) {
-        // const subheading = document.createElement("h3");
-        // subheading.classList.add("subheading");
-        // if (qfunc == 0) {
-        // glass.appendChild(subheading);
-        // glass.querySelector('.subheading').innerHTML = card.querySelector('.subheading').innerHTML;
-        // } 
-        // else {
-        //   let textdata = document.createTextNode(qfunc);
-        //   subheading.appendChild(textdata);
-        //   glass.appendChild(subheading);
-        card.getElementsByClassName('button')[0].style.display = "none";  
-         }
-       // const subheading = card.querySelector('.subheading');
 
-       // glass.appendChild(card.querySelector('.subheading').innerHTML);
-    //}
+        card.getElementsByClassName('button')[0].style.display = "none";  
+
+         }
+
     if (card.getElementsByClassName("stars")[0] != null) {
 
         const stars = createStars();
@@ -59,8 +34,7 @@ const createGlass = (card) => {
         glass.getElementsByClassName("star-box")[0].innerHTML = card.getElementsByClassName("star-box")[1].innerHTML;
         card.getElementsByClassName("star-box")[1].style.display = "none";
     }
-    //card.prepend(glass)
-  /*  return glass;*/
+
 };
 
 export { createGlass };
