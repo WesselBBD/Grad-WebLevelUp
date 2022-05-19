c4e301b2742bb90c09962ee8e9b0ffed62686bec
Wessel Scholtz <wessels@bbd.co.za>
2022-05-19 23:28:30 +0200

    Fixed wack CaseFan names

--- Database/Data/FanData.sql
+++ Database/Data/FanData.sql
@@ -14,8 +14,8 @@ VALUES	('Helix Whirler', 2, 160, 5234, 1450, '/assets/fans/Ceiling Fans/CeilingF
 	('"Little One" Desk Fan', 3, 230, 1843, 224, '/assets/fans/Desk Fans/DeskFan5.png', 2, 6, 6),
 	('Salmon Fury   ', 3, 250, 1530, 280, '/assets/fans/Desk Fans/DeskFan6.png', 1, 43, 43),
 
-	('Lucky‘s Blaster', 4, 160, 370, 150, '/assets/fans/Handheld Fans/Handheld1.png', 4, 6, 20),
-    ('Supa‘ Fast Cooler', 4, 200, 369, 169, '/assets/fans/Handheld Fans/Handheld2.png', 5, 21, 20),
+	('Lucky''s Blaster', 4, 160, 370, 150, '/assets/fans/Handheld Fans/Handheld1.png', 4, 6, 20),
+    ('Supa'' Fast Cooler', 4, 200, 369, 169, '/assets/fans/Handheld Fans/Handheld2.png', 5, 21, 20),
     ('Pircrote', 4, 170, 368, 131, '/assets/fans/Handheld Fans/Handheld3.png', 3, 22, 9),
     ('DP-200 Joint', 4, 298, 369, 160, '/assets/fans/Handheld Fans/Handheld4.png', 2, 23, 24),
     ('Cardinal Sin', 4, 125, 420, 145, '/assets/fans/Handheld Fans/Handheld5.png', 4, 25, 14),
@@ -28,11 +28,11 @@ VALUES	('Helix Whirler', 2, 160, 5234, 1450, '/assets/fans/Ceiling Fans/CeilingF
     ('Vintage TY', 5, 636, 4598, 1001, '/assets/fans/Aircon Fans/Aircon5.png', 4, 32, 32),
     ('PC-RT FG-50', 5, 456, 4598, 943, '/assets/fans/Aircon Fans/Aircon6.png', 4, 33, 33),
 
-    ('TechNine', 6, 499, 4511, 8000, '/assets/fans/Case Fans/CaseFans1.png', 3, 35, 35),
-    ('AZZA FAN INNIT', 6, 879, 3256, 3690, '/assets/fans/Case Fans/CaseFans2.png', 5, 35, 24),
-    ('Titanium White Power', 6, 231, 1569, 2500, '/assets/fans/Case Fans/CaseFans3.png', 3, 36, 8),
-    ('V3SP3R', 6, 123, 3200, 1477, '/assets/fans/Case Fans/CaseFans4.png', 4, 34, 34),
-    ('The BFF', 6, 328, 4789, 8947, '/assets/fans/Case Fans/CaseFans5.png', 5, 34, 34),
+    ('TechNine', 6, 499, 4511, 8000, '/assets/fans/Case Fans/CaseFan1.png', 3, 35, 35),
+    ('AZZA FAN INNIT', 6, 879, 3256, 3690, '/assets/fans/Case Fans/CaseFan2.png', 5, 35, 24),
+    ('Titanium White Power', 6, 231, 1569, 2500, '/assets/fans/Case Fans/CaseFan3.png', 3, 36, 8),
+    ('V3SP3R', 6, 123, 3200, 1477, '/assets/fans/Case Fans/CaseFan4.png', 4, 34, 34),
+    ('The BFF', 6, 328, 4789, 8947, '/assets/fans/Case Fans/CaseFan5.png', 5, 34, 34),
 
     ('PK-400', 7, 20000, 30015, 3689, '/assets/fans/Turbine Fans/Turbine1.png', 4, 37, 37),
     ('Malrovine', 7, 22098, 35001, 3246, '/assets/fans/Turbine Fans/Turbine2.png', 5, 37, 38),
