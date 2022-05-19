2cff3c6b5dea8587e1442e93a5b9647c37e3fbf9
RushilDaya-BBD <rushild@bbd.co.za>
2022-05-19 11:31:37 +0200

    Update cardInfo.js

--- Frontend/components/cardInfo/cardInfo.js
+++ Frontend/components/cardInfo/cardInfo.js
@@ -1,16 +1,28 @@
 const createCardInfo = (card, fanID) => {
 
+    const fanData= {"id":"d74b11ec-598b-7e88-8d27-0eab84de5e5f","FanName":"Metl Wind","FanCategoryName":"Desk Fans","FanRPM":530,"FanCFM":2300,"FanSpanMM":235,"FanImageURL":"https://commons.wikimedia.org/wiki/File:Test.png","FanBaseColourName":"Stainless Steel","FanBaseColourHex":"BDBCBA","FanAccentColourName":"Stainless Steel","FanAccentColourHex":"BDBCBA"}
+
     const cardInfo = document.createElement("section");
     cardInfo.classList.add("cardInfo")
-    // Query database, use FanID and get required info
-    // Query-fanID
-    // Get the Model and MAxRPM from Fan table
-    // WikiContent from Wiki table
 
-    // NB: Can create view for these 3 columns
+    const detailsP= document.createElement("p");
+    let dText = document.createTextNode(fanData.FanRPM + "\n" + fanData.FanCFM + "/n" + fanData.FanSpanMM);
+    detailsP.appendChild(dText);
 
 
-    let info = "FAN INFO" ;
+    const colorsP= document.createElement("p");
+    let colText = document.createTextNode(fanData.FanBaseColourName+ "\n" +  fanData.FanAccentColourName + "\n");
+    colorsP.appendChild(colText);
+
+    cardInfo.appendChild(detailsP);
+    cardInfo.appendChild(colorsP);
+
+
+
+
+
+
+  
 
     let infoText = document.createTextNode(info);
     cardInfo.appendChild(infoText);
