148ee44d782c99ae7dc3df332672774639ce7b7f
Fabio Sousa Vieira <fabio.sousavieira@vcontractor.co.za>
2022-05-19 15:40:11 +0200

    Sends form data to backend

--- Frontend/pages/news-letter.mjs
+++ Frontend/pages/news-letter.mjs
@@ -1,6 +1,6 @@
 import { createHeader } from "../components/header/header.mjs";
 import { createFooter } from "../components/footer/footer.mjs";
-import { createButton } from '../components/button/button.mjs';
+import { createButton } from "../components/button/button.mjs";
 
 const [header] = document.getElementsByTagName("header");
 header.append(...createHeader().children);
@@ -8,45 +8,63 @@ header.append(...createHeader().children);
 const [footer] = document.getElementsByTagName("footer");
 footer.append(...createFooter().children);
 
-const [form] = document.getElementsByTagName('form');
+const [form] = document.getElementsByTagName("form");
 
 const host = "https://zkk9ts3etf.execute-api.af-south-1.amazonaws.com";
-const endpoint = '/dev/api/contact-details';
+const endpoint = "/dev/api/contact-details";
 
 const sendFormData = async (data) => {
   return await fetch(`${host}${endpoint}`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Accept': 'application/json',
-      'Host': host
+      Accept: "application/json",
+      Host: host,
     },
-    body: data
+    body: data,
   });
-}
+};
 
 form.onsubmit = (event) => {
   event.preventDefault();
-  // const data = Object.fromEntries(new FormData(event.target));
 
-  // (async () => {
-  //   console.log(await sendFormData(data));
-  // })();
+  var headers = new Headers();
+  headers.append("Content-Type", "application/json");
 
-  if (true) {
-    const [main] = document.getElementsByTagName('main');
+  const data = Object.fromEntries(new FormData(event.target));
+  const { name, surname, email } = data;
 
-    main.innerHTML = "";
+  var requestOptions = {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify({ name, surname, email, categoryId: "2" }),
+    redirect: "follow",
+  };
+  let success = false;
 
-    const logo = document.createElement('img');
-    logo.setAttribute('src', "../assets/imgs/logo.svg");
+  fetch(
+    "https://zkk9ts3etf.execute-api.af-south-1.amazonaws.com/dev/api/contact-details",
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      const [main] = document.getElementsByTagName("main");
 
-    const heading = document.createElement('h2');
-    heading.innerText = 'Newsletter Success!';
+      main.innerHTML = "";
 
-    const backButton = createButton('BACK TO HOME', () => window.location.href = '../index.html');
+      const logo = document.createElement("img");
+      logo.setAttribute("src", "../assets/imgs/logo.svg");
 
-    main.appendChild(logo);
-    main.appendChild(heading);
-    main.appendChild(backButton);
-  }
-}
+      const heading = document.createElement("h2");
+      heading.innerText = "Newsletter Success!";
+
+      const backButton = createButton(
+        "BACK TO HOME",
+        () => (window.location.href = "../index.html")
+      );
+
+      main.appendChild(logo);
+      main.appendChild(heading);
+      main.appendChild(backButton);
+    })
+    .catch((error) => console.log("error", error));
+};
